05eed91f58ad35fa5fab12696e419a5fe117e865
dink-straycat <dink.straycat@gmail.com>
2013-11-02 23:43:54 +0900

    更新されてた

--- ebook/log-horizon.rb
+++ ebook/log-horizon.rb
@@ -7,9 +7,10 @@ puts "ログ・ホライズン"
 puts "橙乃ままれ"
 puts "\n"
 
-(1..54).each do |i|
+(1..62).each do |i|
   puts %Q!\nThread #{sprintf("%03d",i)}\n\n!
   
+  warn "http://ncode.syosetu.com/n8725k/#{i}/"
   doc = Nokogiri::HTML(open("http://ncode.syosetu.com/n8725k/#{i}/").read)
 
   doc.xpath("//div[@id='novel_view']").inner_text.each_line do |line|
